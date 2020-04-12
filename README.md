f815ed3828e0aa48fea2e6cc6cccb727a3b79960
Leandro Macedo <fmlimao@gmail.com>
2020-04-12 12:21:47 -0300

    Funcionalidade pronta

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# ACL PHP
-Simples ACL em PHP para todos os projetos.
+# ACL PHP - Simples ACL em PHP para todos os projetos.
+
+E quando eu digo simples, é simples mesmo!
+
+## Instalação
+
+    composer require fmlimao/php-acl
+
+## Como usar
+
+Basicamente, basta você associar um Papel a um Recurso e a um Privilégio no momento de dar a permissão ou
+de remover a permissão:
+
+    $acl->allow('Papel', 'Recurso', 'Privilégio');
+    
+    // ou
+    
+    $acl->deny('Papel', 'Recurso', 'Privilégio');
+
+    
+Também é possivel enviar um array em qualquer um dos três parâmetros:
+
+    $acl->allow(['Papel 1', 'Papel 2'], ['Recurso 1', Recurso 2', Recurso 3'], ['Privilégio 1']);
+
+    
+Podemos passar `null` como valor em qualquer um dos parâmetros.
+
+- Se o Papel for `null`, então os Recursos e seus Provilégios serão atribuidos a todos os Papeis.
+- Se o Recurso for `null`, então os Privilégios serão dados a todos os Recursos.
+- Se o Privilégio for `null`, então os Papeis terão todos os Privilégios deste Recurso.
+
+
+    // Todos os Papeis terão esses Privilégios nestes Recursos.
+    $acl->allow(null, ['products', 'categories'], ['list', 'update']);
+
+    // O papel "admin" terá esses Privilégios em todos os Recursos
+    $acl->allow('admin', null, ['create', 'delete']);
+    
+    // O papel "member" terá todos os Privilégios no recurso "orders"
+    $acl->allow('member', 'orders', null);
+
+    
+E para verificar se tem ou não a permissão, basta chamar assim:
+
+    $isAllowed = $acl->isAllowed('Papel', 'Recurso', 'Privilégio');
+
+
+Aqui também é permitido passar arrays como parâmetros: 
+    
+    $isAllowed = $acl->isAllowed(['Papel 1', 'Papel 2'], ['Recurso 1', Recurso 2', Recurso 3'], ['Privilégio 1']);
+
+
+Também é possível passar um array nos argumentos:
+
+    $isAllowed = $acl->isAllowed(['Papel 1', 'Papel 2'], ['Recurso 1', Recurso 2', Recurso 3'], ['Privilégio 1']);
+    
--- src/Acl.php
+++ src/Acl.php
@@ -4,11 +4,18 @@ namespace Fmlimao;
 
 class Acl
 {
+    private $roles = [];
+
     private $rules = [
         'allRoles' => [
+            'type' => 'all',
+
             'allResources' => [
+                'type' => 'all',
+
                 'allPrivileges' => [
-                    'value' => false,
+                    'type' => 'all',
+                    'value' => null,
                 ],
 
                 'byPrivilege' => [],
@@ -20,14 +27,52 @@ class Acl
         'byRole' => [],
     ];
 
+    public function addRole($role, $parent = null)
+    {
+        if (is_string($role)) {
+            if (!isset($this->roles[$role])) {
+                $this->roles[$role] = [
+                    'name' => $role,
+                    'parents' => [],
+                    'children' => [],
+                ];
+            }
+
+            if (!is_null($parent)) {
+                if (is_array($parent)) {
+                    foreach ($parent as $p) {
+                        $this->addRole($role, $p);
+                    }
+                    return;
+                } else if (is_string($parent)) {
+                    $this->addRole($parent);
+
+                    if (!in_array($parent, $this->roles[$role]['parents'])) {
+                        $this->roles[$role]['parents'][] = $parent;
+                    }
+
+                    if (!in_array($role, $this->roles[$parent]['children'])) {
+                        $this->roles[$parent]['children'][] = $role;
+                    }
+                }
+            }
+        }
+    }
+
     private function createNode($role = null, $resource = null)
     {
         if (!is_null($role)) {
             if (!isset($this->rules['byRole'][$role])) {
                 $this->rules['byRole'][$role] = [
+                    'type' => 'role',
+                    'name' => $role,
+
                     'allResources' => [
+                        'type' => 'all',
+
                         'allPrivileges' => [
-                            'value' => false,
+                            'type' => 'all',
+                            'value' => null,
                         ],
 
                         'byPrivilege' => [],
@@ -39,8 +84,12 @@ class Acl
 
             if (!is_null($resource) && !isset($this->rules['byRole'][$role]['byResource'][$resource])) {
                 $this->rules['byRole'][$role]['byResource'][$resource] = [
+                    'type' => 'resource',
+                    'name' => $resource,
+
                     'allPrivileges' => [
-                        'value' => false,
+                        'type' => 'all',
+                        'value' => null,
                     ],
 
                     'byPrivilege' => [],
@@ -49,37 +98,16 @@ class Acl
         } else if (!is_null($resource)) {
             if (!isset($this->rules['allRoles']['byResource'][$resource])) {
                 $this->rules['allRoles']['byResource'][$resource] = [
+                    'type' => 'resource',
+                    'name' => $resource,
+
                     'allPrivileges' => [
-                        'value' => false,
+                        'type' => 'all',
+                        'value' => null,
                     ],
 
                     'byPrivilege' => [],
                 ];
-            }
-        }
-    }
-
-    private function deleteNode($role = null, $resource = null, $privilege = null)
-    {
-        if (is_null($role)) {
-            if (is_null($resource)) {
-                if (!is_null($privilege)) {
-                    unset($this->rules['allRoles']['allResources']['byPrivilege'][$privilege]);
-                }
-            } else {
-                if (!is_null($privilege)) {
-                    unset($this->rules['allRoles']['byResource'][$resource]['byPrivilege'][$privilege]);
-                }
-            }
-        } else {
-            if (is_null($resource)) {
-                if (!is_null($privilege)) {
-                    unset($this->rules['byRole'][$role]['allResources']['byPrivilege'][$privilege]);
-                }
-            } else {
-                if (!is_null($privilege)) {
-                    unset($this->rules['byRole'][$role]['byResource'][$resource]['byPrivilege'][$privilege]);
-                }
             }
         }
     }
@@ -90,20 +118,26 @@ class Acl
             if (is_null($resource)) {
                 if (is_null($privilege)) {
                     $this->rules['allRoles']['allResources']['allPrivileges'] = [
+                        'type' => 'all',
                         'value' => $value,
                     ];
                 } else {
                     $this->rules['allRoles']['allResources']['byPrivilege'][$privilege] = [
+                        'type' => 'privilege',
+                        'name' => $privilege,
                         'value' => $value,
                     ];
                 }
             } else {
                 if (is_null($privilege)) {
                     $this->rules['allRoles']['byResource'][$resource]['allPrivileges'] = [
+                        'type' => 'all',
                         'value' => $value,
                     ];
                 } else {
                     $this->rules['allRoles']['byResource'][$resource]['byPrivilege'][$privilege] = [
+                        'type' => 'privilege',
+                        'name' => $privilege,
                         'value' => $value,
                     ];
                 }
@@ -112,20 +146,26 @@ class Acl
             if (is_null($resource)) {
                 if (is_null($privilege)) {
                     $this->rules['byRole'][$role]['allResources']['allPrivileges'] = [
+                        'type' => 'all',
                         'value' => $value,
                     ];
                 } else {
                     $this->rules['byRole'][$role]['allResources']['byPrivilege'][$privilege] = [
+                        'type' => 'privilege',
+                        'name' => $privilege,
                         'value' => $value,
                     ];
                 }
             } else {
                 if (is_null($privilege)) {
                     $this->rules['byRole'][$role]['byResource'][$resource]['allPrivileges'] = [
+                        'type' => 'all',
                         'value' => $value,
                     ];
                 } else {
                     $this->rules['byRole'][$role]['byResource'][$resource]['byPrivilege'][$privilege] = [
+                        'type' => 'privilege',
+                        'name' => $privilege,
                         'value' => $value,
                     ];
                 }
@@ -151,7 +191,7 @@ class Acl
         }
 
         $this->createNode($role, $resource);
-        $this->setValue($role, $resource, $privilege, true);
+        $this->setValue($role, $resource, $privilege, 1);
     }
 
     public function deny($role = null, $resource = null, $privilege = null)
@@ -171,11 +211,11 @@ class Acl
             return;
         }
 
-        $this->setValue($role, $resource, $privilege, false);
-        $this->deleteNode($role, $resource, $privilege);
+        $this->createNode($role, $resource);
+        $this->setValue($role, $resource, $privilege, 0);
     }
 
-    public function isAllowed($role = null, $resource = null, $privilege = null)
+    public function isAllowed($role = null, $resource = null, $privilege = null, $tab = 0)
     {
         if (is_array($role)) {
             $isAllowedRole = [];
@@ -202,7 +242,24 @@ class Acl
                     $_resource = (is_null($resource) || !isset($_role['byResource'][$resource])) ? $_role['allResources'] : $_role['byResource'][$resource];
                     $_privilege = (is_null($privilege) || !isset($_resource['byPrivilege'][$privilege])) ? $_resource['allPrivileges'] : $_resource['byPrivilege'][$privilege];
 
-                    return $_privilege['value'];
+                    $value = $_privilege['value'];
+
+                    if (!is_null($value)) return !!$value;
+                    
+                    if ($_role['type'] == 'role') {
+                        $roleName = $_role['name'];
+
+                        if (isset($this->roles[$roleName])) {
+                            $parents = $this->roles[$roleName]['parents'];
+                            $value = null;
+                            foreach ($parents as $parent) {
+                                $value = $this->isAllowed($parent, $resource, $privilege, ++$tab);
+                            }
+                            return $value;
+                        }
+                    }
+
+                    return !!$value;
                 }
             }
         }
